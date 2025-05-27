37e66a13354d71ae88db275f0c6ec0d81a213f88
Hemanth Yadav K <109170547+Komarala@users.noreply.github.com>
2025-05-27 18:37:08 +0200

    pdf2excel commit

---
+++ pdf2excel.py
@@ -0,0 +1,22 @@
+import tabula
+import pandas as pd
+
+ADD_NEW_SHEET_FOR_EACH_PAGE_IN_PDF = False
+
+def pdf_to_excel(pdf_file_path, excel_file_path):
+    # Read PDF file
+    tables = tabula.read_pdf(pdf_file_path, pages="1-21", multiple_tables=True)
+
+    if( ADD_NEW_SHEET_FOR_EACH_PAGE_IN_PDF ):
+        # Write each table to a separate sheet in the Excel file
+        with pd.ExcelWriter(excel_file_path) as writer:
+            for i, table in enumerate(tables):
+                table.to_excel(writer, sheet_name=f'Sheet{i+1}', index=False)
+    else:
+        # Concatenate all tables into a single DataFrame
+        combined_table = pd.concat(tables, ignore_index=True)
+        # Write the combined table to a single sheet in the Excel file
+        combined_table.to_excel(excel_file_path, sheet_name='Combined', index=False)
+
+
+pdf_to_excel('./list-of-candidates-2025_26.pdf', './pdf_to_excel_file.xlsx')
